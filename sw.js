81a8f623d80b6ec02c3c7ad0875a62c68c750348
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2022-06-18 19:26:43 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,3 @@
-caches.delete(`paarmycache`);
-caches.delete(`worsourcecache`);
-caches.delete(`pwa-example`);
-caches.delete(`v2`);
-caches.delete(`paarmyV3`);
-caches.delete(`paarmyV5`);
-caches.delete(`paarmyV6`);
-caches.delete(`paarmyV10`);
-caches.delete(`paarmyV13`);
-caches.delete(`paarmyV14`);
-caches.delete(`paarmyV15`);
-caches.delete(`paarmyV17`);
-caches.delete(`paarmyV18`);
-caches.delete(`paarmyV19`);
-caches.delete(`paarmyV20`);
-caches.delete(`paarmyV21`);
-caches.delete(`paarmyV22`);
-caches.delete(`paarmyV23`);
-caches.delete(`paarmyV25`);
-caches.delete(`paarmyV26`);
-
-
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open('paarmyV27').then(function(cache) {

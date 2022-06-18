2210b8983025997a38bee70e26d49a4d1bf433b5
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2022-06-18 19:28:38 -0400

    Update config.php

--- lib/config.php
+++ lib/config.php
@@ -1,6 +1,6 @@
 <?php
-const DB_HOST = '135.148.47.216';
-const DB_USER = 'erinabu';
-const DB_PASS = 'vanlex';
+const DB_HOST = '';
+const DB_USER = '';
+const DB_PASS = '';
 const DB_NAME = 'pennsylvania';
 ?>
